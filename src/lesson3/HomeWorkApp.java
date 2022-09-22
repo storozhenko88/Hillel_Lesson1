14e2075ed2ce6e70080fe4d80aaa4e52df359a6c
storozhenko <storozhenko-88@ukr.net>
2022-09-22 21:32:45 +0300

    Added method printThreeWords

---
+++ src/lesson3/HomeWorkApp.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+}
