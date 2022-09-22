01fb359871984116663479116c2d9c9a0d8cf78f
storozhenko <storozhenko-88@ukr.net>
2022-09-22 21:45:51 +0300

    Added method checkSumSign

--- src/lesson3/HomeWorkApp.java
+++ src/lesson3/HomeWorkApp.java
@@ -4,9 +4,20 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
-        System.out.println("Orange\nBanana\nApple");
+        System.out.println("Orange\nBanana\nApple\n");
+    }
+
+    public static void checkSumSign(){
+        int a = 5;
+        int b = 7;
+
+        if (a+b >= 0)
+            System.out.println("The amount is positive\n");
+        else
+            System.out.println("The amount is negative\n");
     }
 }
