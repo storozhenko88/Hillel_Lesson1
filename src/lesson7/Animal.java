bea592c5cb30e55e0e5d34a54dcfc7d287f456ac
storozhenko <storozhenko-88@ukr.net>
2022-09-30 22:38:27 +0300

    Added class Animal

---
+++ src/lesson7/Animal.java
@@ -0,0 +1,13 @@
+package lesson7;
+
+public class Animal {
+
+    public void run(int distance){
+        System.out.println("Animal running....");
+    }
+
+    public void swim(int distance){
+        System.out.println("Animal swimming....");
+
+    }
+}
