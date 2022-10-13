294d5e36a21c07f6d668d15d05ccf025695a4c39
storozhenko <storozhenko-88@ukr.net>
2022-10-13 18:02:37 +0300

    Added classes PhoneDirectory and Recording

---
+++ src/lesson10/phonebook/PhoneDirectory.java
@@ -0,0 +1,77 @@
+package lesson10.phonebook;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PhoneDirectory {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Recording> directory = new ArrayList<>();
+        int choice;
+
+        do {
+            System.out.print("""
+                    Phone Directory
+                    1 - add contact
+                    0 - exit
+                    """);
+            choice = scanner.nextInt();
+
+            if (choice == 1)
+                directory.add(add());
+
+        } while (choice != 0);
+
+        System.out.println("\nFind person");
+        System.out.println(find(directory));
+        System.out.println("\nFind all:");
+        System.out.println(findALL(directory));
+    }
+
+    public static Recording add() {
+        Scanner scanner = new Scanner(System.in);
+        Recording person = new Recording();
+
+        System.out.print("name: ");
+        person.setName(scanner.nextLine());
+
+        System.out.print("phone number: ");
+        person.setPhoneNumber(scanner.nextLine());
+
+        return person;
+    }
+
+    public static String find(List<Recording> directory) {
+        String findName;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the name for finding: ");
+        findName = scanner.nextLine();
+
+        for (Recording person : directory) {
+            if (person.getName().equals(findName)) {
+                findName += ": phone - " + person.getPhoneNumber();
+                break;
+            }
+        }
+        return findName;
+    }
+
+    public static List<String> findALL(List<Recording> directory) {
+        List<String> findInformation = new ArrayList<>();
+        String findName;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the name for finding: ");
+        findName = scanner.nextLine();
+
+        for (Recording person : directory) {
+            if (person.getName().equals(findName)) {
+                findInformation.add(findName + ": phone - " + person.getPhoneNumber());
+            }
+        }
+        return findInformation;
+    }
+}
---
+++ src/lesson10/phonebook/Recording.java
@@ -0,0 +1,26 @@
+package lesson10.phonebook;
+
+public class Recording {
+
+    private String name;
+    private String phoneNumber;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber1) {
+        this.phoneNumber = phoneNumber1;
+    }
+}
+
+
