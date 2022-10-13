d4c9752f53507876261a28a815db573e4f41a4fd
storozhenko <storozhenko-88@ukr.net>
2022-10-13 14:20:00 +0300

    Added class Task4_2

--- src/lesson10/Task4_2.java
+++ src/lesson10/Task4_2.java
@@ -4,25 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Task4 {
+public class Task4_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("enter the list words:");
         List<String> wordsData = new ArrayList<>();
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 3; i++)
             wordsData.add(scanner.nextLine());
 
-        calcOccurance(wordsData);
+        System.out.println(calcOccurance(wordsData));
     }
 
-    public static void calcOccurance(List<String> wordData) {
+    public static List <String> calcOccurance(List<String> wordData) {
         List<String> wordDataCollector = new ArrayList<>();
+        List<String> finalList = new ArrayList<>();
+        String buffer = "";
 
         int count, lastIndex;
 
         for (int i = 0; i < wordData.size(); i++) {
             count = 0;
+            buffer = "";
             if (!wordDataCollector.contains(wordData.get(i))) {
                 wordDataCollector.add(wordData.get(i));
                 count++;
@@ -30,9 +33,11 @@ public class Task4 {
                 for (int j = i + 1; j < wordData.size(); j++)
                     if (wordDataCollector.get(lastIndex).equals(wordData.get(j)))
                         count++;
-                System.out.println(wordData.get(i) + " - " + count);
+                buffer = "{name: " + wordData.get(i) + ", occurance " + count + "}";
+                finalList.add(buffer);
             }
         }
+        return finalList;
     }
-}
 
+}
